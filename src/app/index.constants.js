046d67785c87dd2b4db4ba1f7b659708757a06ed
zhaoman <906998248@qq.com>
2017-12-07 15:47:43 +0800

    project init

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('gulpAngularKlorofil')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gulpAngularKlorofil', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'toastr']);
+
+})();

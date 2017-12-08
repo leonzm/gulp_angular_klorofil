8c5179af5315d8d61a047ec989b29ddc19213bd6
zhaoman <906998248@qq.com>
2017-12-08 14:22:03 +0800

    公共js 移到 index.run.js

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,9 +6,40 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log, $rootScope) {
 
     $log.debug('runBlock end');
+
+    // 初始化侧边导航栏菜单 active
+    if ($rootScope.sidebarMenuActive == null) {
+      $log.info('初始化侧边导航栏菜单 active');
+
+      $rootScope.sidebarMenuActive = {index: 'active', elements: '', panels: '', subPages: 'collapsed', notifications: '',
+        tables: '', typography: '', icons: ''};
+      $rootScope.menuClick = function (menuName) {
+        $rootScope.sidebarMenuActive = {index: '', elements: '', panels: '', subPages: 'collapsed', notifications: '',
+          tables: '', typography: '', icons: ''};
+
+        if ('index' == menuName) {
+          $rootScope.sidebarMenuActive.index = 'active';
+        } else if ('elements' == menuName) {
+          $rootScope.sidebarMenuActive.elements = 'active';
+        } else if ('panels' == menuName) {
+          $rootScope.sidebarMenuActive.panels = 'active';
+        } else if ('subPages' == menuName) {
+          $rootScope.sidebarMenuActive.subPages = 'active';
+        } else if ('notifications' == menuName) {
+          $rootScope.sidebarMenuActive.notifications = 'active';
+        } else if ('tables' == menuName) {
+          $rootScope.sidebarMenuActive.tables = 'active';
+        } else if ('typography' == menuName) {
+          $rootScope.sidebarMenuActive.typography = 'active';
+        } else if ('icons' == menuName) {
+          $rootScope.sidebarMenuActive.icons = 'active';
+        }
+      };
+
+    }
   }
 
 })();
--- src/app/index/index.controller.js
+++ src/app/index/index.controller.js
@@ -6,39 +6,7 @@
     .controller('IndexController', IndexController);
 
   /** @ngInject */
-  function IndexController($log, $rootScope) {
+  function IndexController($log) {
     $log.info("IndexController init");
-
-    // 初始化侧边导航栏菜单 active
-    if ($rootScope.sidebarMenuActive == null) {
-      $log.info('初始化侧边导航栏菜单 active');
-
-      $rootScope.sidebarMenuActive = {index: 'active', elements: '', panels: '', subPages: 'collapsed', notifications: '',
-        tables: '', typography: '', icons: ''};
-      $rootScope.menuClick = function (menuName) {
-        $rootScope.sidebarMenuActive = {index: '', elements: '', panels: '', subPages: 'collapsed', notifications: '',
-          tables: '', typography: '', icons: ''};
-
-        if ('index' == menuName) {
-          $rootScope.sidebarMenuActive.index = 'active';
-        } else if ('elements' == menuName) {
-          $rootScope.sidebarMenuActive.elements = 'active';
-        } else if ('panels' == menuName) {
-          $rootScope.sidebarMenuActive.panels = 'active';
-        } else if ('subPages' == menuName) {
-          $rootScope.sidebarMenuActive.subPages = 'active';
-        } else if ('notifications' == menuName) {
-          $rootScope.sidebarMenuActive.notifications = 'active';
-        } else if ('tables' == menuName) {
-          $rootScope.sidebarMenuActive.tables = 'active';
-        } else if ('typography' == menuName) {
-          $rootScope.sidebarMenuActive.typography = 'active';
-        } else if ('icons' == menuName) {
-          $rootScope.sidebarMenuActive.icons = 'active';
-        }
-      };
-
-    }
-
   }
 })();

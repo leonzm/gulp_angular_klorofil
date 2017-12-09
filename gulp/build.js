9acbe00122897e89570f6480d5e6a6755b09e574
zhaoman <906998248@qq.com>
2017-12-09 10:03:59 +0800

    解决

--- gulp/build.js
+++ gulp/build.js
@@ -83,6 +83,13 @@ gulp.task('myFonts', function () {
     .pipe(gulp.dest(path.join(conf.paths.dist, '/styles/fonts/')));
 });
 
+gulp.task('myFonts2', function () {
+  return gulp.src('bower_components/font-awesome/fonts/*')
+    .pipe($.filter('**/*.{eot,otf,svg,ttf,woff,woff2}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
+});
+
 gulp.task('other', function () {
   var fileFilter = $.filter(function (file) {
     return file.stat.isFile();
@@ -100,4 +107,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'myFonts', 'other']);
+gulp.task('build', ['html', 'fonts', 'myFonts', 'myFonts2', 'other']);

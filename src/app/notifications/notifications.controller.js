27dd5a46d5fbc3b4cfaa42c88f2fda315b06b0fc
zhaoman <906998248@qq.com>
2018-07-11 11:59:24 +0800

    notifications add toastr test

--- src/app/notifications/notifications.controller.js
+++ src/app/notifications/notifications.controller.js
@@ -6,7 +6,10 @@
     .controller('NotificationsController', NotificationsController);
 
   /** @ngInject */
-  function NotificationsController($log) {
+  function NotificationsController($log, toastr) {
     $log.info("NotificationsController init");
+
+    // toastr 通知测试，按钮事件通知就不一一写了
+    toastr.success('Hello world!', 'Toastr fun!');
   }
 })();
